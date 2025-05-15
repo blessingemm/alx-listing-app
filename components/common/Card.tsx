6c60eb86a5321537b5a01d0f04c6e0f0227791c5
Blessing <eblessing793577@gmail.com>
2025-05-15 14:18:33 +0000

    Add listing app

---
+++ components/common/Card.tsx
@@ -0,0 +1,10 @@
+const Card = () => {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md">
+      <h3 className="text-xl font-semibold">Card Title</h3>
+      <p className="text-gray-600">Card Content</p>
+    </div>
+  );
+};
+
+export default Card;
